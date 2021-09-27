80cc5239dd86df828b138bb83295986cdab66652
qbmemodp <m.ali202038@yahoo.com>
2021-09-27 03:38:39 +0200

    Initial Commit

---
+++ learning gh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learning gh
+//
+//  Created by Mohammed on 9/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learning gh/learning_ghApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_ghApp.swift
+//  learning gh
+//
+//  Created by Mohammed on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct learning_ghApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
